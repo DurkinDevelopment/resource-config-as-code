bc8575dfc5ed1c367c13eec7645fbc83a8f69f64
Chad Durkin <DurkinDevelopment@gmail.com>
2022-09-07 08:42:32 -0700

    feat: Update the template to use escapes

--- src/scripts/create_monitor_function.py
+++ src/scripts/create_monitor_function.py
@@ -8,27 +8,27 @@ def randomword(length):
    return ''.join(random.choice(letters) for i in range(length))
 
 monitor_json_template = """datadog.MonitorJson(\"monitorJson\", monitor=\"\"\"{
-    "name": "%s",
-    "type": "service check",
-    "query": "\"ntp.in_sync\".by(\"*\").last(2).count_by_status()",
-    "message": "Change the message triggers if any host's clock goes out of sync with the time given by NTP. The offset threshold is configured in the Agent's 'ntp.yaml' file.\n\nSee [Troubleshooting NTP Offset issues](https://docs.datadoghq.com/agent/troubleshooting/ntp for more details on cause and resolution.",
-    "tags": [],
-    "multi": true,
-	"restricted_roles": null,
-    "options": {
-        "include_tags": true,
-        "locked": false,
-        "new_host_delay": 150,
-        "notify_audit": false,
-        "notify_no_data": false,
-        "thresholds": {
-            "warning": 1,
-            "ok": 1,
-            "critical": 1
+    \"name\": \"%s\",
+    \"type\": \"service check\",
+    \"query\": \"\"ntp.in_sync\".by(\"*\").last(2).count_by_status()\",
+    \"message\": \"Change the message triggers if any host's clock goes out of sync with the time given by NTP. The offset threshold is configured in the Agent's 'ntp.yaml' file.\n\nSee [Troubleshooting NTP Offset issues](https://docs.datadoghq.com/agent/troubleshooting/ntp for more details on cause and resolution.\",
+    \"tags\": [],
+    \"multi\": true,
+	\"restricted_roles\": null,
+    \"options\": {
+        \"include_tags\": true,
+        \"locked\": false,
+        \"new_host_delay\": 150,
+        \"notify_audit\": false,
+        \"notify_no_data\": false,
+        \"thresholds\": {
+            \"warning\": 1,
+            \"ok\": 1,
+            \"critical\": 1
         }
     },
-    "priority": null,
-    "classification": "custom"
+    \"priority\": null,
+    \"classification\": \"custom\"
 }
 \"\"\")"""
 
